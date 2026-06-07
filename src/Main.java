450acbb685d671ae7209005426508b862a4d34b2
WajihaZubair <wajihazubair96@gmail.com>
2026-06-07 22:07:23 +0300

    Added prototype class

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,20 @@
 import com.courseware.components.*;
+import com.courseware.prototype.*;
 
 public class Main{
     public static void main(String[] args){
         Chapters chapter1=new Chapters("Introdcution");
         chapter1.add(new LessonText("What is OOP?"));
         chapter1.add(new QuizBlocks("Define encapsulation"));
+        chapter1.add(new Diagrams("OOP Class Diagram"));
+        chapter1.add(new VideoLinks("youtube.com/oop-intro"));
+        System.out.println("---Display chapter components---");
         chapter1.display();
+
+        System.out.println("\n---Testing Prototype pattern (cloning)---");
+        courseTemplate template=new courseTemplate("101 Course Template");
+        courseTemplate oopCourse=(courseTemplate)template.clone();
+        oopCourse.display();
     }
 
 
--- src/com/courseware/prototype/courseTemplate.java
+++ src/com/courseware/prototype/courseTemplate.java
@@ -1,4 +1,28 @@
 package com.courseware.prototype;
 
-public class courseTemplate {
+//clone a 101 course template
+//a reusable course template that can be duplicated....provides a cloning mechanism, for creating copies of the course template
+public class courseTemplate implements Cloneable{
+
+    private String courseName;
+
+    //creates a course template with the specified course name
+    public courseTemplate(String courseName){
+        this.courseName=courseName;
+    }
+
+    //creates and returns a copy of this course template
+    @Override
+    public courseTemplate clone(){
+        try{
+            return(courseTemplate) super.clone();
+        }
+        catch(CloneNotSupportedException e){
+            throw new RuntimeException((e));
+        }
+    }
+    public void display(){
+        System.out.println(courseName);
+    }
+
 }
